4a667ae03effdd15d87a9778bc9caffeb3df1dc2
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-06-30 23:59:31 +0300

    newer commit

--- backend/index.ts
+++ backend/index.ts
@@ -9,4 +9,8 @@ connectDB().then(() => {
   app.listen(PORT, () => {// start the express server
     console.log("✅Server is running on PORT:", PORT);
   });
+})
+.catch((error) => {
+  console.error("failed to start server", error);
+  process.exit(1);
 });
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI environment variable  is not defined");
+    }
+
+    await mongoose.connect(mongoUri);
     console.log("✅ MongoDb connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error", error);
