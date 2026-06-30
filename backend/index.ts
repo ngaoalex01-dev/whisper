88c37d52bf13facf1e79fcb5ccc928cd8cc461b9
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-06-30 23:48:03 +0300

    DataBase/Routes set up

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+
+import "dotenv/config";
+import app from "./src/app";
+import { connectDB } from "./src/config/database";
+
+const PORT = process.env.PORT;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {// start the express server
+    console.log("✅Server is running on PORT:", PORT);
+  });
+});
